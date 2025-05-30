badde22123b31201dc3a6e54da4a90f6bded9d8f
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-05-30 22:12:04 +0530

    feat(mock): add sample emails data

---
+++ data/data.go
@@ -0,0 +1,21 @@
+package data
+
+var Emails = map[int]string{
+	1001: "alice@example.com",
+	1002: "bob@example.com",
+	1003: "carol@example.com",
+	1004: "dave@example.com",
+	1005: "eve@example.com",
+	1006: "frank@example.com",
+	1007: "grace@example.com",
+	1008: "heidi@example.com",
+	1009: "ivan@example.com",
+	1010: "judy@example.com",
+	1011: "mallory@example.com",
+	1012: "nina@example.com",
+	1013: "oscar@example.com",
+	1014: "peggy@example.com",
+	1015: "trent@example.com",
+	1016: "victor@example.com",
+	1017: "wendy@example.com",
+}
