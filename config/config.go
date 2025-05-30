f36061257bddf4b6cee3c3ff80dfb2f79ccd13f2
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-05-30 22:32:10 +0530

    feat/config: add config for tree storage

--- config/config.go
+++ config/config.go
@@ -1,1 +1,13 @@
 package config
+
+type Config struct {
+	TreeOrder   int
+	StoragePath string
+}
+
+func LoadConfig() *Config {
+	return &Config{
+		TreeOrder:   4,
+		StoragePath: "data/tree.db",
+	}
+}
